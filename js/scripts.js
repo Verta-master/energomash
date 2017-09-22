97b0852a7b1da33261c2ba6e00af77212af9a21d
Verta-master <vertyagina@gmail.com>
2017-09-22 18:40:27 +0600

    Main files

---
+++ js/scripts.js
@@ -0,0 +1,20 @@
+﻿$(document).ready(function() {
+
+	$(".modal-link").on("click", function(){
+		var modal = $(this).data("link");
+		$("."+modal).arcticmodal({
+			overlay: {
+				css: {
+					opacity: .9
+				}
+			}
+		});
+		return false;
+	});
+
+	// MODAL NAV
+	$(".modal-close").on("click", function() {
+		$.arcticmodal("close");
+	});
+	
+});
